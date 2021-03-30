8e35cc63cbc7f0d31b0dc81392d68c5ff8cc6c96
AtriTripathi <tripathi.atri@gmail.com>
2021-03-31 03:55:19 +0530

    Add LoadState handling for network requests.

--- app/src/main/java/com/atritripathi/facade/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/atritripathi/facade/ui/gallery/GalleryFragment.kt
@@ -5,8 +5,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
+import androidx.paging.LoadState.*
 import com.atritripathi.facade.R
 import com.atritripathi.facade.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,16 +29,40 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         val photoAdapter = UnsplashPhotoAdapter()
 
-        with(binding.rvImageGallery) {
-            adapter = photoAdapter.withLoadStateHeaderAndFooter(
-                header = UnsplashPhotoLoadStateAdapter { photoAdapter.retry() },
-                footer = UnsplashPhotoLoadStateAdapter { photoAdapter.retry() }
-            )
-            setHasFixedSize(true)
+        with(binding) {
+            rvImageGallery.apply {
+                adapter = photoAdapter.withLoadStateHeaderAndFooter(
+                    header = UnsplashPhotoLoadStateAdapter { photoAdapter.retry() },
+                    footer = UnsplashPhotoLoadStateAdapter { photoAdapter.retry() }
+                )
+                itemAnimator = null
+                setHasFixedSize(true)
+            }
+            btnRetry.setOnClickListener { photoAdapter.retry() }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             photoAdapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        photoAdapter.addLoadStateListener { loadState ->
+            with(binding) {
+                progressBar.isVisible = loadState.source.refresh is Loading
+                rvImageGallery.isVisible = loadState.source.refresh is NotLoading
+                btnRetry.isVisible = loadState.source.refresh is Error
+                tvError.isVisible = loadState.source.refresh is Error
+
+                // When there are no results
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    photoAdapter.itemCount < 1
+                ) {
+                    rvImageGallery.isVisible = false
+                    tvEmpty.isVisible = true
+                } else {
+                    tvEmpty.isVisible = false
+                }
+            }
         }
 
         setHasOptionsMenu(true)
