f38dd791c8271ac1781fc8d1dc903353c12ce6cc
AtriTripathi <tripathi.atri@gmail.com>
2021-04-01 06:32:14 +0530

    Add Process Death handling using `SavedStateHandle`.

--- app/src/main/java/com/atritripathi/facade/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/atritripathi/facade/ui/gallery/GalleryViewModel.kt
@@ -1,24 +1,27 @@
 package com.atritripathi.facade.ui.gallery
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.atritripathi.facade.data.UnsplashRepository
+import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    state: SavedStateHandle
 ) : ViewModel() {
 
     companion object {
-        private const val DEFAULT_QUERY = "cats"
+        private const val KEY_CURRENT_QUERY = "current_query_key"
+        private const val DEFAULT_QUERY = "dogs"
     }
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(KEY_CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResult(queryString).cachedIn(viewModelScope)
